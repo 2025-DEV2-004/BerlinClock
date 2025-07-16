b6e24383498b8a37b27996184c6f8c68c3cf08c4
faridmarzouki <farid.marzouki@argenta.be>
2025-07-16 13:54:52 +0200

    Add basic implementation for the screen components and add preview functions

--- app/src/debug/kotlin/screens/PreviewBerlinClockScreen.kt
+++ app/src/debug/kotlin/screens/PreviewBerlinClockScreen.kt
@@ -1,8 +1,20 @@
 package screens
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.exercise.berlinclock.model.FiveMinuteRowState
+import com.exercise.berlinclock.model.FourRowState
+import com.exercise.berlinclock.model.LampState
 import com.exercise.berlinclock.ui.screens.BerlinClockScreen
+import com.exercise.berlinclock.ui.screens.ClockLamp
+import com.exercise.berlinclock.ui.screens.FiveMinuteRow
+import com.exercise.berlinclock.ui.screens.FourLampRow
+import com.exercise.berlinclock.ui.theme.BerlinClockTheme
 
 @Preview
 @Composable
@@ -10,4 +22,77 @@ private fun PreviewBerlinClockScreen() {
     // This is where the Berlin Clock UI will be implemented
     // For now, we can leave it empty or add a placeholder
     BerlinClockScreen()
+}
+
+@Preview
+@Composable
+private fun PreviewClockLamp() {
+    BerlinClockTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            ClockLamp(modifier = Modifier, lampState = true)
+            ClockLamp(modifier = Modifier, lampState = false)
+            ClockLamp(modifier = Modifier, lampState = false)
+        }
+
+    }
+}
+
+@Preview(uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun PreviewDarkClockLamp() {
+    BerlinClockTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            ClockLamp(modifier = Modifier, lampState = true)
+            ClockLamp(modifier = Modifier, lampState = false)
+            ClockLamp(modifier = Modifier, lampState = false)
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewFourLampRow() {
+    BerlinClockTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            FourLampRow(
+                lamps = FourRowState(
+                    lamps = listOf(true, false, false, false)
+                )
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewFiveMinuteRow() {
+    BerlinClockTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            FiveMinuteRow(
+                lamps = FiveMinuteRowState(
+                    listOf(
+                        LampState.YELLOW,
+                        LampState.YELLOW,
+                        LampState.RED,
+                        LampState.OFF,
+                        LampState.OFF,
+                        LampState.OFF,
+                        LampState.OFF,
+                        LampState.OFF,
+                        LampState.OFF,
+                        LampState.OFF,
+                        LampState.OFF
+                    )
+                )
+            )
+        }
+    }
 }
--- app/src/main/kotlin/com/exercise/berlinclock/ui/screens/BerlinClockScreen.kt
+++ app/src/main/kotlin/com/exercise/berlinclock/ui/screens/BerlinClockScreen.kt
@@ -1,7 +1,22 @@
 package com.exercise.berlinclock.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.exercise.berlinclock.model.FiveMinuteRowState
+import com.exercise.berlinclock.model.FourRowState
+import com.exercise.berlinclock.model.LampState
 
 @Composable
 internal fun BerlinClockScreen() {
@@ -9,3 +24,85 @@ internal fun BerlinClockScreen() {
     // For now, we can leave it empty or add a placeholder
     Text("Berlin Clock Screen")
 }
+
+@Composable
+internal fun BerlinClockContent() {
+    Scaffold { internalPadding ->
+        Column(modifier = Modifier.padding(internalPadding)) {
+        }
+    }
+}
+
+
+@Composable
+internal fun ClockLamp(
+    modifier: Modifier = Modifier,
+    lampState: Boolean,
+) {
+    Box(
+        modifier = modifier
+            .height(lampsSize)
+            .background(
+                color = if (lampState) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.tertiary,
+                shape = MaterialTheme.shapes.large
+            )
+    )
+}
+
+
+@Composable
+internal fun ClockLamp(
+    modifier: Modifier = Modifier,
+    lampState: LampState
+) {
+    Box(
+        modifier = modifier
+            .height(lampsSize)
+            .background(
+                color = when (lampState) {
+                    LampState.OFF -> MaterialTheme.colorScheme.tertiary
+                    LampState.YELLOW -> MaterialTheme.colorScheme.secondary
+                    LampState.RED -> MaterialTheme.colorScheme.primary
+                },
+            )
+    )
+}
+
+
+@Composable
+internal fun FourLampRow(
+    modifier: Modifier = Modifier,
+    lamps: FourRowState,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        lamps.lamps.forEach { lamp ->
+            ClockLamp(
+                modifier = Modifier.weight(1f),
+                lampState = lamp
+            )
+        }
+    }
+}
+
+@Composable
+internal fun FiveMinuteRow(
+    modifier: Modifier = Modifier,
+    lamps: FiveMinuteRowState,
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        lamps.lamps.forEach { lamp ->
+            ClockLamp(
+                modifier = Modifier.weight(1f),
+                lampState = lamp
+            )
+        }
+    }
+}
+
+private val lampsSize = 50.dp
